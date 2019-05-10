794e0006d22bcfaa46aa5b88f7187cfc385b1a12
jiaxiangkai <jiaxk@zenmen.com>
2019-05-10 18:25:03 +0800

    webpack  init

---
+++ build/webpack.config.js
@@ -0,0 +1,8 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpack = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: ''
+}
