09423011700ba27b6a07c345f059ad8e6948508b
jiaxiangkai <jiaxk@zenmen.com>
2019-05-24 19:34:34 +0800

    fetch修改

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import Fetch from 'services/fetch'
 // import Footer from 'layouts/Footer'
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.getData()
+  }
+  getData = () => {
+    Fetch(
+      '/api?format=json&v=v1&method=category.itemCategory'
+    ).then(res => {
+      console.log(res)
+    })
+    
+  }
   render() {
     return (
       // <Footer />
--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,101 +1,91 @@
-const typeMap = {
+const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+const contentTypes = {
   'application/json': 'json',
   'text/html': 'text',
   'Blob/File': 'blob',
   'FormData': 'formData',
   'ArrayBuffer': 'arrayBuffer'
 }
-const methods = ['GET','PUT','PATCH','DELETE','POST'];
-function handleResponse(response) {
-  let contentType = response.headers.get('content-type');
-  let responseType = 'text';
-  for (let key in typeMap) {
-    if (contentType.includes(key)) {
-      responseType = typeMap[key];
+//fetch返回的回调也是promise对象,判断返回的content-type
+function dealResponse(response) {
+  const contentType = response.headers.get('content-type');
+  let resType = 'json'; //返回的数据流格式
+  for(let key in contentTypes) {
+    if(contentType.indexOf(key) > -1) {
+      resType = contentTypes[key]
     }
   }
-  console.log('response', response)
-  return response[responseType]()
-    .then(data => {
-      if (response.ok) {
-        return data;
+  return response[resType]().then(data => {
+    if(response.ok) {
+      return data
+    } else {
+      return reject(`${response.status}---${response.statusText}`)
+    }
+  })
+}
+
+//timeout处理
+function promiseTimeout(time, callback) {
+  let timer = null;  
+  let timeout_promise = new Promise((resolve, reject) => {
+    timer = setTimeout(() => {
+      reject('请求超时')
+    }, time)
+  })
+
+  Promise.race([callback, timeout_promise]).then(() => {
+    window.clearTimeout(timer)
+  })
+}
+
+/**
+ * timeout不是请求连接超时的含义，它表示请求的response时间，包括请求的连接、服务器处理及服务器响应回来的时间；
+ * fetch的timeout即使超时发生了，本次请求也不会被abort丢弃掉，仍然会发送到服务器端，只是本次请求的响应内容被丢弃而已s；
+ */
+function Fetch(url, opt) {
+  if(!opt) opt = {};
+  if(!opt.timeout) opt.timeout = 10000;
+  if(url && typeof url !== 'string') return;
+  // if(opt.method && !methods.some(m => m == method.toUpperCase())) return;
+  if (opt.data) {
+    //post 设置headers,body
+    if(method.toUpperCase() == 'POST') {
+      if(opt.data instanceof FormData) {
+        opt.headers['Content-Type'] = 'multipart/form-data;'
+      } else if(opt.data instanceof Object) {
+        opt.body = JSON.stringify(opt.data)
       } else {
-        return Promise.reject({
-          status: response.status,
-          statusText: response.statusText,
-          errorBody: data
-        })
+        opt.headers['Content-Type'] = 'application:/x-www-form-urlencoded:charset=UTF-8'
+      }
+    } else {
+      const searchParam = url.indexOf('?') > -1 ? '' : '?';
+      url += searchParam + Object.keys(opt.data).map(key => `${key}=${encodeURIComponent(options.data[key])}`).join('&');
+    }
+    delete opt.data;
+  }
+
+  //fetch外封装一层promise
+  return new Promise((resolve, reject) => {
+    let baseParams = {
+      method: 'get',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    }
+    let reqParams = Object.assign(baseParams, opt)
+    const fecth_promise = fetch(url, reqParams).then(dealResponse).then(data => {
+      if(data.errorcode == 0) {
+        resolve(data.data)
+        console.log(data.data)
+      } else {
+        reject('请求出错')
       }
     })
+    //设置请求超时
+    promiseTimeout(opt.timeout, fecth_promise)
+  })
 }
 
-export default function Fetch(url, options) {
-
-    if (url && typeof url !== 'string') return;
-
-    if (options && typeof options !== 'object') return;
-
-    
-    if (options && options.method && !methods.some(m => m == options.method.toUpperCase())) {
-       return false;
-    }
-    if (options && options.method.toUpperCase() == 'POST' && options.data) {
-      if(options.data instanceof FormData){
-        options.headers['Content-Type'] = 'multipart/form-data;';
-      } else if (options.data instanceof Object) {
-        options.body = JSON.stringify(options.data);
-      } else {
-        options.headers['Content-Type'] = 'application:/x-www-form-urlencoded:charset=UTF-8';
-      }
-      delete options.data;
-    } else if (options && options.data) {
-      url += '?' + Object.keys(options.data).map((key) => {
-        return `${key}=${encodeURIComponent(options.data[key])}`;
-      }).join('&');
-      delete options.data;
-    }
-    return new Promise((resolve, reject) => {
-      let myRequest = Object.assign({
-        method: 'GET',
-        mode: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: 'same-origin',
-        cache: 'default'
-      }, options),
-      fetch_promise = fetch(url, myRequest)
-      .then(handleResponse)
-      .then((data) => {
-        if (data.status == 0) {
-          resolve(data);
-        } else {
-          reject(data);
-        }
-      }),
-      abort_function = () => {
-        reject('abort');
-        console.log('abort done');
-      },
-      timer = null,
-      timeout_function = (time) => {
-        if (time = +time) {
-          timer = setTimeout(abort_function, time);
-          console.log('start timeout');
-        }
-      },
-      sequence = [fetch_promise];
-      console.log('myRequest', myRequest);
-      if (options && options.timeout) {
-        let timeout_promise = new Promise((resolve, reject) => {
-          timeout_function(options.timeout);
-        });
-        sequence.push(timeout_promise);
-      }
-      Promise.race(sequence).then(() => {
-        window.clearTimeout(timer);
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-}
+export default Fetch
