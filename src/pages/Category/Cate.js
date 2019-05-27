87db0f2bece0c1e36514b5c0cf98cd45efb81516
Jack <285316882@qq.com>
2019-05-27 23:33:27 +0800

    prettier formate

--- src/pages/Category/Cate.js
+++ src/pages/Category/Cate.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react'
-import Fetch from 'services/fetch'
-import cs from 'classnames'
-import './Cate.scss'
+import React, { Component } from "react";
+import Fetch from "services/fetch";
+import cs from "classnames";
+import "./Cate.scss";
 // import Footer from 'layouts/Footer'
 
 class Catetory extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   componentDidMount() {
-    this.getData()
+    this.getData();
   }
   getData = async () => {
-    const res = await Fetch('/api?format=json&v=v1&method=category.itemCategory')
-    console.log(res)
+    const res = await Fetch(
+      "/api?format=json&v=v1&method=category.itemCategory"
+    );
+    console.log(res);
+  };
 
-  }
-  
   render() {
     return (
       <div className="cate-wrap">
@@ -24,14 +25,14 @@ class Catetory extends Component {
           <div className="cate-title">搜索</div>
           <div className="cate-input-wrap">
             <div className="cate-input-cantent">
-              <span className="icon icon-ic_sousuo"></span>
+              <span className="icon icon-ic_sousuo" />
               <span>点击进行搜索</span>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Catetory
+export default Catetory;
--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,41 +1,41 @@
-const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+const methods = ["GET", "POST", "PUT", "DELETE", "PATCH"];
 const contentTypes = {
-  'application/json': 'json',
-  'text/html': 'text',
-  'Blob/File': 'blob',
-  'FormData': 'formData',
-  'ArrayBuffer': 'arrayBuffer'
-}
+  "application/json": "json",
+  "text/html": "text",
+  "Blob/File": "blob",
+  FormData: "formData",
+  ArrayBuffer: "arrayBuffer"
+};
 //fetch返回的回调也是promise对象,判断返回的content-type
 function dealResponse(response) {
-  const contentType = response.headers.get('content-type');
-  let resType = 'json'; //返回的数据流格式
-  for(let key in contentTypes) {
-    if(contentType.indexOf(key) > -1) {
-      resType = contentTypes[key]
+  const contentType = response.headers.get("content-type");
+  let resType = "json"; //返回的数据流格式
+  for (let key in contentTypes) {
+    if (contentType.indexOf(key) > -1) {
+      resType = contentTypes[key];
     }
   }
   return response[resType]().then(data => {
-    if(response.ok) {
-      return data
+    if (response.ok) {
+      return data;
     } else {
-      return reject(`${response.status}---${response.statusText}`)
+      return reject(`${response.status}---${response.statusText}`);
     }
-  })
+  });
 }
 
 //timeout处理
 function promiseTimeout(time, callback) {
-  let timer = null;  
+  let timer = null;
   let timeout_promise = new Promise((resolve, reject) => {
     timer = setTimeout(() => {
-      reject('请求超时')
-    }, time)
-  })
+      reject("请求超时");
+    }, time);
+  });
 
   Promise.race([callback, timeout_promise]).then(() => {
-    window.clearTimeout(timer)
-  })
+    window.clearTimeout(timer);
+  });
 }
 
 /**
@@ -43,23 +43,28 @@ function promiseTimeout(time, callback) {
  * fetch的timeout即使超时发生了，本次请求也不会被abort丢弃掉，仍然会发送到服务器端，只是本次请求的响应内容被丢弃而已s；
  */
 function Fetch(url, opt) {
-  if(!opt) opt = {};
-  if(!opt.timeout) opt.timeout = 10000;
-  if(url && typeof url !== 'string') return;
-  if(opt.method && !methods.some(m => m == method.toUpperCase())) return;
+  if (!opt) opt = {};
+  if (!opt.timeout) opt.timeout = 10000;
+  if (url && typeof url !== "string") return;
+  if (opt.method && !methods.some(m => m == method.toUpperCase())) return;
   if (opt.data) {
     //post 设置headers,body
-    if(method.toUpperCase() == 'POST') {
-      if(opt.data instanceof FormData) {
-        opt.headers['Content-Type'] = 'multipart/form-data;'
-      } else if(opt.data instanceof Object) {
-        opt.body = JSON.stringify(opt.data)
+    if (method.toUpperCase() == "POST") {
+      if (opt.data instanceof FormData) {
+        opt.headers["Content-Type"] = "multipart/form-data;";
+      } else if (opt.data instanceof Object) {
+        opt.body = JSON.stringify(opt.data);
       } else {
-        opt.headers['Content-Type'] = 'application:/x-www-form-urlencoded:charset=UTF-8'
+        opt.headers["Content-Type"] =
+          "application:/x-www-form-urlencoded:charset=UTF-8";
       }
     } else {
-      const searchParam = url.indexOf('?') > -1 ? '' : '?';
-      url += searchParam + Object.keys(opt.data).map(key => `${key}=${encodeURIComponent(options.data[key])}`).join('&');
+      const searchParam = url.indexOf("?") > -1 ? "" : "?";
+      url +=
+        searchParam +
+        Object.keys(opt.data)
+          .map(key => `${key}=${encodeURIComponent(options.data[key])}`)
+          .join("&");
     }
     delete opt.data;
   }
@@ -67,25 +72,27 @@ function Fetch(url, opt) {
   //fetch外封装一层promise
   return new Promise((resolve, reject) => {
     let baseParams = {
-      method: 'get',
-      mode: 'no-cors',
+      method: "get",
+      mode: "no-cors",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
-      credentials: 'include'
-    }
-    let reqParams = Object.assign(baseParams, opt)
-    const fecth_promise = fetch(url, reqParams).then(dealResponse).then(data => {
-      if(data.errorcode == 0) {
-        resolve(data.data)
-        // console.log(data.data)
-      } else {
-        reject('请求出错')
-      }
-    })
+      credentials: "include"
+    };
+    let reqParams = Object.assign(baseParams, opt);
+    const fetch_promise = fetch(url, reqParams)
+      .then(dealResponse)
+      .then(data => {
+        if (data.errorcode == 0) {
+          resolve(data.data);
+          // console.log(data.data)
+        } else {
+          reject("请求出错");
+        }
+      });
     //设置请求超时
-    promiseTimeout(opt.timeout, fecth_promise)
-  })
+    promiseTimeout(opt.timeout, fetch_promise);
+  });
 }
 
-export default Fetch
+export default Fetch;
