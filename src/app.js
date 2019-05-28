c4899025114f0841d69f47b7d3220c002f20400e
jiaxiangkai <jiaxk@zenmen.com>
2019-05-28 19:50:14 +0800

    init  page animation

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
-import ReactDom from 'react-dom'
-import 'assets/style/reset.scss'
-import 'assets/style/index.scss'
-import AppRouter from 'router/index'
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+import "assets/style/reset.scss";
+import "assets/style/index.scss";
+import AppRouter from "router/index";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class App extends Component {
   render() {
-    return <AppRouter/>
+    return (
+      <TransitionGroup>
+        <CSSTransition
+          timeout={500}
+          classNames="fade"
+          unmountOnExit
+          appear={true}
+        >
+          <AppRouter />
+        </CSSTransition>
+      </TransitionGroup>
+    );
+    // return <AppRouter />;
   }
 }
 
-export default App
+export default App;
